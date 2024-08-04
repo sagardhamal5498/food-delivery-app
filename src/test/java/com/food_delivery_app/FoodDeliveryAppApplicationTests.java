80f5c64cb6dd12056fa13abc2c7dccc0705e43c5
sagardhamal5498 <sagardhamal9999@gmail.com>
2024-08-04 10:44:59 +0530

    Main Project

---
+++ src/test/java/com/food_delivery_app/FoodDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
